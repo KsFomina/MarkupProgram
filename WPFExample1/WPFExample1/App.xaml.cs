6970110919468235913dd1b28ce4a60c09221c5b
sarkatvas <katyasarycheva03@gmail.com>
2024-03-19 21:13:48 +0300

    это супер пупер не работающий проект с кнопочками,

---
+++ WPFExample1/WPFExample1/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MahApps.Metro.Behaviors;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using WPFExample1.Models;
+using WPFExample1.Views;
+using WPFExample1.Module;
+
+namespace WPFExample1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell() => Container.Resolve<Window>();
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry
+            .RegisterSingleton<Window>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<MainModule>();
+        }
+    }
+}
---
+++ WPFExample1/WPFExample1/Module/MainModule.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Modularity;
+using Prism.Ioc;
+using Prism.Regions;
+using System;
+using WPFExample1.Views;
+
+namespace WPFExample1.Module
+{
+    internal class MainModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var region = containerProvider.Resolve<IRegionManager>();
+            region.RegisterViewWithRegion("MainRegion", typeof(TabView));  //TabView is the view or UserControl which will inject to Region
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
