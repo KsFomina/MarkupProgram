a3ec98a718094546e40a334e4c5f8de144b78ef6
KsFomina <136327241+KsFomina@users.noreply.github.com>
2024-03-20 22:40:52 +0300

    подключила библиотеку для БД

---
+++ ProDich/ProDich/База данных/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Data.SqlClient;
+
+namespace ProDich.База_данных
+{
+    internal class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("");
+    }
+}
