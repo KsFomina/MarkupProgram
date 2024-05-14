0dc643241fc70df3ee5eb548ad6813ef31e2f8b4
KsFomina <sloveckija@gmail.com>
2024-05-14 15:36:42 +0300

    поломаная история

---
+++ AutomaticMarkup/AutomaticMarkup.Layout/DataBase/BaseConnection.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticMarkup.Layout.DataBase
+{
+    public class BaseConnection
+    {
+        string connectionString = "Data Sourse=DESKTOP-T89823G\\SQLEXPRESS; Database=History; user=root; Integrated Security=True";
+
+        //public BaseConnection()
+        //{
+        //    MySqlConnectionStringBuilder con = new MySqlConnectionStringBuilder();
+        //    con.Server = "localhost";
+        //    con.UserID = "root";
+        //    con.Database = "History";
+        //    con.Password = "";
+
+
+        //    connectionString = con.ToString();
+        //}
+        MySqlConnection connection;
+
+        public void openConnection()
+        {
+            if (connection==null || connection.State==System.Data.ConnectionState.Closed) {
+
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+            }
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public MySqlConnection getConnectionString() {
+            return connection;
+        }
+
+        public DataTable getData()
+        {
+            return connection.GetSchema("history");
+        }
+    }
+}
--- AutomaticMarkup/AutomaticMarkup.Layout/ViewModels/StoryViewModel.cs
+++ AutomaticMarkup/AutomaticMarkup.Layout/ViewModels/StoryViewModel.cs
@@ -1,17 +1,37 @@
 ﻿using AutomaticMarkup.Layout;
+using AutomaticMarkup.Layout.DataBase;
 using Prism.Commands;
 using Prism.Regions;
 using ReactiveUI;
+using System.ComponentModel;
+using System.Data;
 using System.Windows.Input;
 
 namespace AutomaticMarkup.ViewModels
 {
     class StoryViewModel: ReactiveObject
+
     {
         private readonly IRegionManager _regionManager;
+        private DataTable dataTable = new DataTable();
+        public DataTable DataTable
+        {
+            get
+            {
+                return dataTable;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref dataTable, value);
+            }
+        }
 
         public StoryViewModel()
         {
+            BaseConnection db = new BaseConnection();
+            db.openConnection();
+            dataTable=db.getData();
+            db.closeConnection();
         }
 
         public StoryViewModel(IRegionManager regionManager)
