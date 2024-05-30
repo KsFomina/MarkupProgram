66fd1cf59ec72369436cfe100394741d7f37f110
BikinDev <bikin@DESKTOP-Q33GQP9>
2024-05-30 14:51:28 +0300

    WorkWithJson

---
+++ AutomaticMarkup/AutoMarkingLib/MarkPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AutoMarkingLib
+{
+	public class MarkPoint
+	{
+		public MarkPoint(System.Drawing.Point p)
+		{
+			X = p.X; 
+			Y = p.Y;
+		}
+		[JsonInclude]
+		public int X;
+		[JsonInclude]
+		public int Y;
+	}
+}
